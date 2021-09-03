f518a546a3c3f044077db5ae0c9577f92f1e0bc4
Clausewitz <67420710+clausewittz@users.noreply.github.com>
2021-09-03 15:30:59 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#Leap Year Check
+year = int(input("Which year do you want to check? "))
+
+#Leap Year Conditions
+if year % 4 == 0: 
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+
